a4624146e7580c94e309d77a8e0f67cc91e16b15
kaky <lachapelle.kaky@gmail.com>
2021-03-17 23:42:56 +0300

    remove confcomponent in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ConfigOpComponent } from './config-op/config-op.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConfigOpComponent,
   ],
   imports: [
     BrowserModule,
